95c7496f7c8b77c70a209b1cf65bc5a5ee48e471
Alex Williams and Patricia Filipovic <patricia@>
2018-10-16 10:24:32 -0700

    add color buttons to toggle between and header fade

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+    $("#header1").fadeToggle();
+  });
   $(".clickable").click(function() {
     $("#show").slideToggle();
     $("#hide").slideToggle();
