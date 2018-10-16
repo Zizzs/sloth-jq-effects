30829d648675cf1909eda88c3d2325f36b94152c
Alex Williams and Patricia Filipovic <patricia@>
2018-10-16 11:45:28 -0700

    Added terms and concepts flashcards for javascript

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,61 @@ $(document).ready(function() {
     $("#show").slideToggle();
     $("#hide").slideToggle();
   });
+  $("#clickable1").click(function() {
+    $("#fadeJS").fadeToggle();
+  });
+  $("#clickable2").click(function() {
+    $("#fadeOp").fadeToggle();
+  });
+  $("#clickable3").click(function() {
+    $("#fadeVar").fadeToggle();
+  });
+  $("#clickable4").click(function() {
+    $("#fadeVarN").fadeToggle();
+  });
+  $("#clickable5").click(function() {
+    $("#fadeFu").fadeToggle();
+  });
+  $("#clickable6").click(function() {
+    $("#fadeMe").fadeToggle();
+  });
+  $("#clickable7").click(function() {
+    $("#fadePa").fadeToggle();
+  });
+  $("#clickable8").click(function() {
+    $("#fadeRe").fadeToggle();
+  });
+  $("#clickable9").click(function() {
+    $("#fadeCm").fadeToggle();
+  });
+  $("#clickable10").click(function() {
+    $("#fadeSt").fadeToggle();
+  });
+  $("#clickable11").click(function() {
+    $("#fadeBo").fadeToggle();
+  });
+  $("#clickable12").click(function() {
+    $("#fadeUn").fadeToggle();
+  });
+  $("#clickable13").click(function() {
+    $("#fadeNaN").fadeToggle();
+  });
+  $("#clickable14").click(function() {
+    $("#fadeEs").fadeToggle();
+  });
+  $("#clickable15").click(function() {
+    $("#fadeAl").fadeToggle();
+  });
+  $("#clickable16").click(function() {
+    $("#fadeCo").fadeToggle();
+  });
+  $("#clickable17").click(function() {
+    $("#fadeJQ").fadeToggle();
+  });
+  $("#clickable18").click(function() {
+    $("#fadeAtt").fadeToggle();
+  });
+  $("#clickable19").click(function() {
+    $("#fadeArg").fadeToggle();
+  });
 });
